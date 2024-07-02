550e5dca3899f5efcbfb255647004d331251ea9c
vanparfait <nguemechiaparfait@gmail.com>
2024-07-02 13:52:42 +0100

    creation de la feature cart

---
+++ src/features/cart.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+};
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    createCartItem: (state, action) => {},
+  },
+});
+
+export function addOneToCart(action) {
+  return function (dispatch, getState) {
+    const storeState = getState();
+
+    const isAlreadyPresent = storeState.cart.cartItems.find(
+      (el) => el.id === action
+    );
+
+    if (!isAlreadyPresent) {
+      const itemToAdd = storeState.products.items.find(
+        (el) => el.id === action
+      );
+
+      const newCartItem = {
+        ...itemToAdd,
+        quantity: 1,
+      };
+
+      dispatch(createCartItem(newCartItem));
+    }
+  };
+}
+
+export const { createCartItem } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import products from "./features/products";
+import cart from "./features/cart";
 
 export const store = configureStore({
   reducer: {
     products,
+    cart,
   },
 });
